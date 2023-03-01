39128c0d4c67f67b0082823890d5a81b2397e457
JEONG MIN <112797177+JeongMiiiin@users.noreply.github.com>
2023-03-01 15:33:50 +0900

    [Silver II] Title: N과 M (12), Time: 408 ms, Memory: 29148 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+
+public class Main {
+	static int N, M;
+	static LinkedHashSet<String> ans = new LinkedHashSet<>();
+	static int[] inputs, numbers;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		
+		inputs = new int[N];
+		numbers = new int[M];
+		for(int i=0; i < N; i++) inputs[i] = sc.nextInt();
+		Arrays.sort(inputs);
+		
+		comb(0,0);
+		ans.forEach(System.out::println);
+		sc.close();
+	}
+	private static void comb(int cnt, int start) {
+		if(cnt == M) {
+			StringBuilder sb = new StringBuilder();
+			for(int i=0; i < M;i++) sb.append(numbers[i] + " ");
+			ans.add(sb.toString());
+			return;
+		}
+		
+		for(int i=start; i < N; i++) {
+			
+			numbers[cnt] = inputs[i];
+			comb(cnt + 1, i);
+		}
+	}
+}
