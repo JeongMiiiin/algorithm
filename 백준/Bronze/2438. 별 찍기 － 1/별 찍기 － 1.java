9645700b9750958ee9b4da826b24a9c2d3fe3e0c
JEONG MIN <112797177+JeongMiiiin@users.noreply.github.com>
2023-03-01 15:04:35 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 128 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i=0; i < N; i++) {
+			sb.append("*");
+			System.out.println(sb.toString());
+		}
+			
+	}
+}
