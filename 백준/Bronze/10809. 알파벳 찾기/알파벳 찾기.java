c6a26f50308ba98f1dbd49008d62faf956d45aa2
JEONG MIN <112797177+JeongMiiiin@users.noreply.github.com>
2023-03-01 15:25:55 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 148 ms, Memory: 15920 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String S = br.readLine();
+		
+		String[] alphabet = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+		
+		for(int i=0; i < alphabet.length; i++)
+			System.out.print(S.indexOf(alphabet[i]) + " ");
+		
+			
+	}
+}
