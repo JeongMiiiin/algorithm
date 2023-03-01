b85bb6923d27f4f52b1199dc6ebf1db8760ec6f9
JEONG MIN <112797177+JeongMiiiin@users.noreply.github.com>
2023-03-01 15:30:40 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 2212 ms, Memory: 186644 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+//백준 11651번 좌표 정렬하기 2
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int[][] map = new int[N][2];
+		for(int i=0; i < N; i++) {
+			map[i][0] = sc.nextInt();
+			map[i][1] = sc.nextInt();
+		}
+		
+		Arrays.sort(map, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] n1, int[] n2) {
+				if(n1[1] == n2[1]) return n1[0] - n2[0];
+					
+				return n1[1] - n2[1];
+			}
+		});
+		
+		for(int i=0; i < N; i++)
+			System.out.println(map[i][0] + " " + map[i][1]);
+		
+	}
+}
