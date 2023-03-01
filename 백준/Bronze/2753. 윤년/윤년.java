f5a27a114a3f9b8579e98825cb334d9c25fa0ed0
JEONG MIN <112797177+JeongMiiiin@users.noreply.github.com>
2023-03-01 15:15:13 +0900

    [Bronze V] Title: 윤년, Time: 208 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int y = sc.nextInt();
+		
+		int result = 0;
+		if(y % 4 == 0)
+			if(y % 100 != 0 || y % 400 == 0) result = 1;
+		
+		System.out.println(result);
+	}
+}
