7ce40e42390c109ebde16d6d1e9a16fb1dc90cd2
JEONG MIN <112797177+JeongMiiiin@users.noreply.github.com>
2023-03-22 23:15:40 +0900

    [Gold V] Title: 연속합 2, Time: 276 ms, Memory: 24524 KB -BaekjoonHub

--- 백준/Gold/13398. 연속합 2/연속합 2.java
+++ 백준/Gold/13398. 연속합 2/연속합 2.java
@@ -25,21 +25,25 @@ public class Main {
 		int[] dp = new int[N + 1];
 		int[] dp2 = new int[N + 1];
 		
+		//첫번째부터 i번째까지의 연속합중 최댓값 저장
 		dp[1] = V[1];
 		for(int i=2; i <= N; i++) {
 			dp[i] = V[i];
 			dp[i] = Math.max(dp[i - 1] + V[i], dp[i]);
 		}
 		
+		//마지막부터 i번째까지의 연속합중 최대값 저장
 		dp2[N] = V[N];
 		for(int i= N - 1; i >= 1; i--) {
 			dp2[i] = V[i];
 			dp2[i] = Math.max(dp2[i], dp2[i + 1] + dp2[i]);
 		}
 		
+		//값을 제외하지 않고의 연속합 최대값 앞에서부터 찾기
 		int ans = dp[1];
 		for(int i=2; i <= N; i++) ans = Math.max(dp[i], ans);
 		
+		//i값을 제외하고 연속합 최대값 찾기
 		for(int i=1; i <= N - 1; i++) ans = Math.max(dp[i - 1] + dp2[i + 1], ans);
 		
 		System.out.println(ans);
