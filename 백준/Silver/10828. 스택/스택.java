34f97d1b02ac624e24db2d92e8f67e96c2408421
JEONG MIN <112797177+JeongMiiiin@users.noreply.github.com>
2023-03-23 13:24:41 +0900

    [Silver IV] Title: 스택, Time: 216 ms, Memory: 19696 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -2,41 +2,69 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
+	static class ArrayStack {
+		int top, size;
+		int [] stack;
+		public ArrayStack(int size) {
+			this.size = size;
+			this.stack = new int[size];
+			this.top = -1;
+		}
+		public void push(int item) {
+			stack[++top] = item;
+		}
+		public int pop() {
+			int pop = stack[top];
+			stack[top--] = 0;
+			return pop;
+		}
+		public int peek() {
+			return stack[top];
+		}
+		public int size() {
+			return top + 1;
+		}
+		public boolean empty() {
+			return top + 1 == 0;
+		}
+	}
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));	
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	
-	public static void main(String[] args) throws Exception{
-		int T = Integer.parseInt(br.readLine());
-		Stack<Integer> stack = new Stack<Integer>();
-		StringBuilder sb = new StringBuilder();
-		for(int t=0; t < T; t++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			String command = st.nextToken();
-			
-			switch(command) {
-				case "push" :
-					stack.push( Integer.parseInt(st.nextToken()) );
+	public static void main(String[] args) throws Exception {
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		String cmd;
+		int target;
+		ArrayStack customStack = new ArrayStack(n);
+		for(int i=0; i < n; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			cmd = st.nextToken();
+			switch(cmd) {
+				case "push":
+					target = Integer.parseInt(st.nextToken());
+					customStack.push(target);
+					break;	
+				case "pop":
+					if(!customStack.empty()) bw.write(customStack.pop() + "\n");
+					else bw.write(-1 + "\n");
 					break;
-				case "pop" :
-					if( !stack.isEmpty() ) System.out.println(stack.pop());
-					else System.out.println(-1);
+				case "size":
+					bw.write(customStack.size() + "\n");
 					break;
-				case "size" :
-					System.out.println(stack.size());
+				case "empty":
+					if(customStack.empty()) bw.write(1 + "\n");
+					else bw.write(0 + "\n");
 					break;
-				case "empty" :
-					if( !stack.isEmpty() ) System.out.println(0);
-					else System.out.println(1);
-					break;
-				case "top" :
-					if( !stack.isEmpty() ) System.out.println(stack.peek());
-					else System.out.println(-1);
+				case "top":
+					if(!customStack.empty()) bw.write(customStack.peek() + "\n");
+					else bw.write(-1 + "\n");
 					break;
 			}
 		}
+		bw.close();
 	}
 }
