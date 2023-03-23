6b18623a25ba224705117e9733ec734b3e53f106
JEONG MIN <112797177+JeongMiiiin@users.noreply.github.com>
2023-03-23 09:05:45 +0900

    [level 2] Title: 문자열 압축, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.java
@@ -2,6 +2,7 @@ class Solution {
     public int solution(String s) {
         int answer = s.length();
         
+        //가장 큰 단위가 2분의 1이기 때문에 해당 개수로 비교
         for(int i = 1; i <= s.length() / 2; i++){
             int zipLevel = 1;
             String zipStr = s.substring(0,i);
@@ -14,13 +15,13 @@ class Solution {
                 if(zipStr.equals(next)) zipLevel++;
                 // 다음 문자와 현재 문자가 다를 경우
                 else{
-                    // (압축이 안됬을 경우는 공백, 압축이 됬을경우 zipLevel을 붙여줌) + 압축할 문자를 넣어줌, 
+                    // (압축이 안되었을 경우는 공백, 압축이 되었을경우 zipLevel을 붙여줌) + 압축할 문자를 넣어줌, 
                     result.append((zipLevel != 1 ? zipLevel : "") + zipStr);
                     zipStr = next;      // 다음 문자를 압축할 문자로 지정
                     zipLevel = 1;       // 압축 정도 1로 초기화
                 }
             }
-            result.append(zipStr);      // 마지막에 추가안된 zipStr추가
+            result.append(zipStr);      // 마지막에 추가 안된 zipStr추가
             answer = Math.min(answer, result.length()); // 가장 작은 문자열 길이 저장
         }
         
