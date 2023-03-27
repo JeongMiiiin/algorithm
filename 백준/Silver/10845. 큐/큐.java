deb83aa4d548fadc314c6bb7c2d0968ea12cc95f
JEONG MIN <112797177+JeongMiiiin@users.noreply.github.com>
2023-03-27 11:51:01 +0900

    [Silver IV] Title: 큐, Time: 212 ms, Memory: 18604 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -2,46 +2,86 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayDeque;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	static StringTokenizer st;
+	static class ArrayQueue {
+		int front, rear, size;
+		int [] queue;
+		public ArrayQueue(int size) {
+			this.front = this.rear = -1;
+			this.size = size;
+			this.queue = new int[size];
+		}
+		public boolean isEmpty() {
+			return front == rear;
+		}
+		public boolean isFull() {
+			return rear == size - 1;
+		}
+		public int size() {
+			return rear - front;
+		}
+		public void offer(int value) {
+			if(isFull()){
+				System.out.println("Queue is Full");
+				return;
+			}
+			queue[++rear] = value;
+		}
+		public int poll() {
+			return queue[++front];
+		}
+		public int front() {
+			return queue[front + 1];
+		}
+		public int back() {
+			return queue[rear];
+		}
+		public void clear() {
+			front = rear = -1;
+			queue = new int[size];
+		}
+	}
 	
 	public static void main(String[] args) throws Exception {
-		int T = Integer.parseInt(br.readLine());
-		Queue<Integer> q = new ArrayDeque<Integer>();
-		int tail = -1;
-		outer : for(int t=0; t < T; t++) {
-			int res = -1;
-			st = new StringTokenizer(br.readLine());
-			String cmd = st.nextToken();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		int n = Integer.parseInt(br.readLine());
+		ArrayQueue q = new ArrayQueue(n);
+		String cmd;
+		int target;
+		for(int i=0; i < n; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			cmd = st.nextToken();
 			switch(cmd) {
-				case "push" :
-					res = Integer.parseInt(st.nextToken());
-					q.offer(res);
-					tail = res;
-					continue outer;
-				case "pop" :
-					if( !q.isEmpty() ) res = q.poll();
+				case "push":
+					target = Integer.parseInt(st.nextToken());
+					q.offer(target);
 					break;
-				case "size" :
-					res = q.size();
+				case "pop":
+					if( !q.isEmpty() ) bw.write(q.poll() + "\n");
+					else bw.write(-1 + "\n");
 					break;
-				case "empty" :
-					res = q.isEmpty() ? 1 : 0;
+				case "size":
+					bw.write(q.size() + "\n");
 					break;
-				case "front" :
-					if( !q.isEmpty() ) res = q.peek();
+				case "empty":
+					if(q.isEmpty()) bw.write(1 + "\n");
+					else bw.write(0 + "\n");
 					break;
-				case "back" :
-					if( !q.isEmpty() ) res = tail;
+				case "front":
+					if( !q.isEmpty() ) bw.write(q.front() + "\n");
+					else bw.write(-1 + "\n");
+					break;
+				case "back":
+					if( !q.isEmpty() ) bw.write(q.back() + "\n");
+					else bw.write(-1 + "\n");
 					break;
 			}
-			System.out.println(res);
 		}
+		
+		bw.close();
 	}
 }
