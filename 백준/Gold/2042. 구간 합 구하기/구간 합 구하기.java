b1e0792de2234bde5b15c8038d9d877c14e6b9bd
JEONG MIN <112797177+JeongMiiiin@users.noreply.github.com>
2023-04-07 13:44:05 +0900

    [Gold I] Title: 구간 합 구하기, Time: 2028 ms, Memory: 342104 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -22,11 +22,16 @@ public class Main {
 		int N = sc.nextInt();
 		int M = sc.nextInt();
 		int K = sc.nextInt();
-		//트리 생성
-		SegmentTree tree = new SegmentTree(N);
-		long[] initArr = new long[N + 1];
-		for(int i=1; i <= N; i++) initArr[i] = sc.nextLong();
-		tree.init(initArr, 1, 1, N);
+		
+		long[] origin = new long[N + 1];
+		FenwickTree tree = new FenwickTree(N + 1);
+		
+		//업데이트
+		for(int i=1; i <= N; i++) {
+			origin[i] = sc.nextLong();
+			tree.update(i, origin[i]);
+		}
+		
 		
 		StringBuilder sb = new StringBuilder();
 		//변경 혹은 구간 합 구하기
@@ -36,84 +41,45 @@ public class Main {
 			b = sc.nextLong();
 			c = sc.nextLong();
 			//변경 시
-			if(a == 1) tree.updateDirect(1, 1, N, b, c);
+			if(a == 1) {
+				tree.update((int) b, c - origin[(int) b]);
+				origin[(int) b] = c;
+			}
 			//구간합 구하기
-			else sb.append(tree.sum(1, 1, N, b, c) + "\n");
+			else sb.append(tree.getRange((int) b, (int) c) + "\n");
 		}
 		
 		System.out.println(sb.toString());
 		sc.close();
 	}
 	
-	static class SegmentTree {
+	static class FenwickTree {
 		//세그먼트 트리를 구현할 배열
-		private long[] tree;
+		public long[] data;
 
 		//생성자에서 세그먼트 트리의 전체노드 수 계산 
-		public SegmentTree(int n) {
-			//트리 높이 계산
-			double treeHei = Math.ceil(Math.log(n) / Math.log(2)) + 1;
-			//트리 노드 수 계산
-			long treeNodeCnt = Math.round(Math.pow(2,  treeHei));
-			//트리 길이 실정
-			tree = new long[Math.toIntExact(treeNodeCnt)];
+		public FenwickTree(int n) {
+			data = new long[n];
 		}
 		
-		/*
-		 * arr -> 세그먼트 트리로 나타낼 배열, node -> 메서드를 시작할 노드 인덱스
-		 * start -> 노드들이 가지는 값의 시작 인덱스, end -> 노드들이 가지는 값의 종료 인덱스
-		 * 자식 노드 번호 (좌측) = 부모 노드 번호 * 2
-		 * 자식 노드 번호 (우측) = (부모 노드 번호 * 2) + 1
-		*/
-		long init(long[] arr, int node, int start, int end) {
-			//세그먼트 트리의 리프노드인 경우
-			//리프 노드에 배열의 값 저장 후 리턴
-			if(start == end) return tree[node] = arr[start]; 
-			//세그먼트 트리의 리프노드가 아닌 경우
-			// 자식 노드의 값을 더해서 노드의 값 초기화 후 리턴
-			else return tree[node] = init(arr, node * 2, start, (start + end) / 2)
-					                 + init(arr, node * 2 + 1, (start + end) / 2 + 1, end); 
-		}
-		
-		long sum(int node, int start, int end, long left, long right) {
-			//노드가 가지는 값의 구간이 구하려고 하는 합의 구간에 속하지 않는 경우 0리턴
-			if(end < left || right < start) return 0;
-			//노드가 가지는 값의 구간이 구하려고 하는 합의 구간에 속하는 경우 노드 값 리턴
-			else if (left <= start && end <= right) return tree[node];
-			/*
-			 * 2가지 경우가 존재
-			 * 1. 노드가 가지는 값의 구간이 구하려고 하는 합의 구간에 일부는 속하고 일부는 속하지 않는 경우
-			 * 2. 노드가 가지는 값의 구간이 구하려고 하는 합의 구간을 모두 포함하는 경우
-			 * 이와 같은 경우에는 자식 노드를 탐색해서 값을 리턴
-			*/
-			else return sum(node * 2, start, (start + end) / 2, left, right)
-					    + sum(node * 2 + 1, (start + end) / 2 + 1, end, left, right);
-		}
-		
-		//배열에 특정 인덱스의 값이 변경될 경우 세그먼트 트리의 노드 값 변경 (차이 값을 더하는 방법)
-		void updateDiff(int node, int start, int end, long idx, long diff) {
-			//노드가 가지는 값의 구간에 변경될 인덱스가 포함되지 않을 경우
-			if(idx < start || end < idx) return;
-			else {
-				//노드가 가지는 값의 구간에 변경될 인덱스가 포함되는 경우
-				tree[node] = tree[node] + diff;
-				//노드가 리프 노드가 아닌 경우
-				if(start != end) {
-					//리프 노드까지 계속 자식 노드를 탐색
-					updateDiff(node * 2, start, (start + end) / 2, idx, diff); //왼쪽 자식 노드
-					updateDiff(node * 2 + 1, (start + end) / 2 + 1, end, idx, diff); //왼쪽 자식 노드
-				}
+		public void update(int idx, long value) {
+			while(idx < data.length) {
+				data[idx] += value;
+				idx += (idx & -idx);
 			}
 		}
-		//배열에 특정 인덱스의 값이 변경될 경우 세그먼트 트리의 노드 값 변경 (노드 값을 직접 변경)
-		long updateDirect(int node, int start, int end, long idx, long changeValue) {
-			//노드가 가지는 값의 구간에 변경될 인덱스가 포함되지 않을 경우
-			if(idx < start || end < idx) return tree[node];
-			//노드가 가지는 값의 구간과 변경될 인덱스 값이 같은 경우
-			else if(start == idx && end == idx) return tree[node] = changeValue;
-			//노드가 가지는 값의 구간에 변경될 인덱스가 포함되는 경우
-			else return tree[node] = updateDirect(node * 2, start, (start + end) / 2, idx, changeValue)
-									 + updateDirect(node * 2 + 1, (start + end) / 2 + 1, end, idx, changeValue);
+		
+		public long sum(int idx) {
+			long result = 0;
+			while(idx > 0) {
+				result += data[idx];
+				idx -= (idx & -idx);
+			}
+			return result;
+		}
+		
+		public long getRange(int start, int end) {
+			return sum(end) - sum(start - 1);
 		}
 	}
 	
