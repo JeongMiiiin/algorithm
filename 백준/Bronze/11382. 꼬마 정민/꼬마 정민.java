43d3b631036022e5b2e17048f7c5c2cc2702ad37
JEONG MIN <112797177+JeongMiiiin@users.noreply.github.com>
2023-05-02 10:20:19 +0900

    [Bronze V] Title: 꼬마 정민, Time: 208 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+/*
+ * 백준 11382 - 꼬마 정민
+*/
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextLong() + sc.nextLong() + sc.nextLong());
+		sc.close();
+	}
+}
