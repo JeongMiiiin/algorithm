042b74ee29edd744a435b32f41d4032c31af7a1c
JEONG MIN <112797177+JeongMiiiin@users.noreply.github.com>
2023-05-23 23:24:33 +0900

    [unrated] Title: 요격 시스템, Time: 0.53 ms, Memory: 70.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+/*
+요격 시스템
+
+*/
+
+class Solution {
+    public int solution(int[][] targets) {
+        
+        Arrays.sort(targets, (o1, o2) -> { return o1[1]-o2[1]; });
+        int answer = 0;
+        int last = -1;
+        for (int[] target : targets) {
+            if (last == -1) {
+                answer++;
+                last = target[1] - 1;
+                continue;
+            }
+            
+            if (last >= target[0] && last <= target[1]) continue;
+            
+            answer++;
+            last = target[1] - 1;
+        }
+        
+        
+        return answer;
+    }
+}
