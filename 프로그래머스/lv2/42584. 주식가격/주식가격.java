2ced7cb42caad930c56015be19afe1689f7c7d7c
JEONG MIN <112797177+JeongMiiiin@users.noreply.github.com>
2023-06-17 21:55:00 +0900

    [level 2] Title: 주식가격, Time: 12.54 ms, Memory: 66.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.java
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -4,19 +4,10 @@ class Solution {
     public int[] solution(int[] prices) {
         int[] answer = {};
         answer = new int[prices.length];
-        for(int i = 0; i < prices.length; ++i)
-        {
-            for(int j = i+1; j < prices.length; ++j)
-            {
-                if(prices[i] <= prices[j])
-                    answer[i]++;
-                else
-                {
-                    //if(answer[i] == 0)
-                        answer[i]++;
-                    break;
-                }
-
+        for(int i = 0; i < prices.length; i++){
+            for(int j = i+1; j < prices.length; j++) {
+                answer[i]++;
+                if(prices[i] > prices[j]) break;
             }
         }
 
