0613974be6a8856ff63a7d27bfbe08acb46c696a
JEONG MIN <112797177+JeongMiiiin@users.noreply.github.com>
2023-06-28 21:50:27 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.06 ms, Memory: 51.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12899. 124 나라의 숫자/124 나라의 숫자.java
@@ -0,0 +1,25 @@
+/*
+11 -> 42
+12 -> 44
+13 -> 111
+*/
+
+class Solution {
+    public String solution(int n) {
+        StringBuilder answer = new StringBuilder();
+
+        while (n > 0) {
+            int num = n % 3;
+            if (num == 0) {
+                answer.insert(0, "4"); 
+                n = n / 3 - 1;
+            } else {
+                answer.insert(0, Integer.toString(num));
+                n = n / 3;
+            }
+
+        }
+
+        return answer.toString();
+    }
+}
