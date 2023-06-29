4b23f8580589acbd964b3865ce718237bb8a8f9a
JEONG MIN <112797177+JeongMiiiin@users.noreply.github.com>
2023-06-30 08:45:10 +0900

    [level 2] Title: 3 x n 타일링, Time: 0.23 ms, Memory: 51.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12902. 3 x n 타일링/3 x n 타일링.java
@@ -0,0 +1,26 @@
+/*
+1 -> 0
+2 -> 3
+3 -> 0
+4 -> 11
+5 -> 0
+
+*/
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        if (n % 2 == 1) return answer;
+
+        long[] dp = new long[n + 1];
+        dp[2] = 3;
+        long sum = 0;
+
+        for (int i = 4; i <= n; i += 2) {
+            dp[i] = (dp[i - 2] * 3 + (sum * 2 + 2)) % 1000000007L;
+            sum += dp[i - 2] % 1000000007L;
+        }
+
+        return (int) dp[n];
+    }
+}
