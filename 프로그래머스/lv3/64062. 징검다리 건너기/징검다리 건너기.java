d1420a2818b6bf573dca5e3712243d3506197a7e
JEONG MIN <112797177+JeongMiiiin@users.noreply.github.com>
2023-07-04 19:54:00 +0900

    [level 3] Title: 징검다리 건너기, Time: 43.85 ms, Memory: 66.4 MB -BaekjoonHub

--- 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -1,25 +1,20 @@
+import java.util.ArrayDeque;
+
 class Solution {
     public int solution(int[] stones, int k) {
-        int min = 0;
-        int max = Integer.MAX_VALUE;
-        int answer = 0;
-        while(min <= max) {
-            int mid = (min + max) / 2;
-            if(check(mid, k, stones)) {
-                min = mid + 1;
-                answer = mid;
-            } else max = mid - 1;
-        }
-        return answer;
-    }
-    
-    public boolean check(int mid, int k, int[] stones) {
-        int count = 0;
-        for(int i = 0; i < stones.length; i++) {
-            if(stones[i] < mid) { //mid보다 작으면 mid가 건널 수 없다.
-                if(++count >= k) return false;
-            } else count = 0;
-        }
-        return true;
+      int answer = Integer.MAX_VALUE;
+        
+      ArrayDeque<int[]> dq = new ArrayDeque<>();
+
+      for(int i = 0; i < stones.length; i++) {
+         int[] temp = {i, stones[i]};
+         int cur = stones[i];
+         if(!dq.isEmpty() && dq.peek()[0] <= temp[0] - k) dq.pollFirst();
+         while(!dq.isEmpty() && dq.peekLast()[1] <= temp[1]) dq.pollLast();
+         dq.add(temp);
+         if(temp[0] >= k - 1) answer = Math.min(answer, dq.peekFirst()[1]);
+      }
+
+      return answer;
     }
 }
