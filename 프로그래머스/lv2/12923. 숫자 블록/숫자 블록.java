0c9e960447b1cb2741b457fc8271e289c078d201
JEONG MIN <112797177+JeongMiiiin@users.noreply.github.com>
2023-07-06 09:10:08 +0900

    [level 2] Title: 숫자 블록, Time: 94.12 ms, Memory: 52.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12923. 숫자 블록/숫자 블록.java
+++ 프로그래머스/lv2/12923. 숫자 블록/숫자 블록.java
@@ -1,30 +1,28 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+/*
+소수인 경우에는 1
+소수가 아닌 경우 약수 중 자신을 제외한 최댓값 (단, 10000000 이하여야 함)
+*/
 
 class Solution {
     public int[] solution(long begin, long end) {
         int[] answer = new int[(int)(end - begin) + 1];
-        for(int i= (int) begin, idx = 0; i <= (int) end; i++, idx++) answer[idx] = getMaxDivisorExceptMe(i);
+        for(int i= (int) begin, idx = 0; i <= (int) end; i++, idx++) answer[idx] = find(i);
         return answer;
     }
     
-    private static int getMaxDivisorExceptMe(int n) {
+    private static int find(int n) {
         if (n == 1) return 0;
 
-        List<Integer> temp = new ArrayList<>();
-        
-        
+        int result = 0;
         
         for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) {
-                temp.add(i);
+                result = Math.max(result, i);
                 if (n / i <= 10000000) return n / i;
             }
 
         }
-        if (!temp.isEmpty()) return temp.get(temp.size() - 1);
-
+        if(result > 0) return result;
         return 1;
     }
 }
