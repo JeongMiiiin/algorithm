c188409ae62d78c72c18c277eb0b20ee0545e5b2
JEONG MIN <112797177+JeongMiiiin@users.noreply.github.com>
2023-07-07 17:49:20 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.04 ms, Memory: 66.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,8 +1,7 @@
 class Solution {
     public long solution(long n) {
         double remain = Math.sqrt(n);
-        int rm = (int) remain;
-        if(remain != rm) return -1;
-        return (long) Math.pow(remain + 1, 2);
+        if(remain != (int) remain) return -1; //정수가 아닌 경우
+        return (long) Math.pow(remain + 1, 2); //1더해서 제곱
     }
 }
