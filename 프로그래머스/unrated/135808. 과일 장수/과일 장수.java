04c24e5aaf83b85c856734a1d68d4eab4d26f08e
JEONG MIN <112797177+JeongMiiiin@users.noreply.github.com>
2023-07-16 23:04:07 +0900

    [unrated] Title: 과일 장수, Time: 0.35 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        Arrays.sort(score);
+        int answer = 0;
+        int idx = score.length;
+        while(idx - m >= 0){
+            answer += score[idx - m] * m;
+            idx -= m;
+        }
+        
+        return answer;
+    }
+}
