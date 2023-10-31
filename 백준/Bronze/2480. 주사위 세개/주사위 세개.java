ddcfa6d9eaa6309fc86af7379f23373182213f0a
JEONG MIN <112797177+JeongMiiiin@users.noreply.github.com>
2023-11-01 01:06:08 +0900

    [Bronze IV] Title: 주사위 세개, Time: 208 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		int result = 0;
+		if(A == B && B == C) result += 10000 + A * 1000;
+		else {
+			if(A == B) result += 1000 + A * 100;
+			else if(A == C) result += 1000 + A * 100;
+			else if(B == C) result += 1000 + B * 100;
+			else result += Math.max(Math.max(A, B), C) * 100;
+		}
+		
+		System.out.println(result);
+		sc.close();
+	}
+}
