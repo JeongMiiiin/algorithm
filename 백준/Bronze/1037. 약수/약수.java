425ea8d5354c1526b6f4c73fbd8c86210b774426
JEONG MIN <112797177+JeongMiiiin@users.noreply.github.com>
2023-11-08 12:08:52 +0900

    [Bronze I] Title: 약수, Time: 212 ms, Memory: 17808 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] nList = new int[N];
+		for(int i=0; i < N; i++) nList[i] = sc.nextInt();
+		Arrays.sort(nList);
+		
+		System.out.println(nList[0] * nList[N - 1]);
+		
+		sc.close();
+	}
+}
